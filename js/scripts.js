86785fea07cecde14dd46caf704f4e3ed6cdeadf
Jesica Quinones <jessqp@jesicas-air.myfiosgateway.com>
2021-12-13 11:33:13 -0500

    updating text

--- js/scripts.js
+++ js/scripts.js
@@ -51,7 +51,7 @@ window.addEventListener('DOMContentLoaded', event => {
     var numInputs = document.querySelectorAll('input');
     const button = document.getElementById('submitButton');
     const log = document.getElementById('values');
-    button.disabled = true 
+    button.disabled = true
     let notValid = true
 
     for (var i = 0; i < numInputs .length; i++) {
@@ -70,74 +70,6 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
 
-    //------------------------------------------
-      // let notValid = false
-    // const input = document.querySelectorAll('input');
-    // const log = document.getElementById('values');
-    // const button = document.getElementById('submitButton');
-
-    // input.addEventListener('input', updateValue);
-    // button.addEventListener('button', updateValue);
-    // notValid = true
-
-    // function updateValue(e) {
-    //     itemMaps.forEach((values,keys)=>{
-    //         log.textContent = (e.target.value.toUpperCase() === 'S');
-    //         if (document.getElementById(keys).value == values){
-    //             button.disabled = false
-    //         }
-    //     })
-    //     log.textContent = (e.target.value.toUpperCase() === 'N');
-    // }
-    // button.disabled = notValid
-
-    // let elementsArray = document.querySelectorAll("whatever");
-    //-----------------------------------------------------------------------
-  
-    // const disableButton = true; //change this value to false and the button will be clickable
-    // const button = document.getElementById('submitButton');
-    
-    // if (disableButton) button.disabled = "disabled";
-
-    // input.addEventListener("change", stateHandle);
-
-    // function stateHandle() {
-    //     for (key in itemMaps.keys()){
-    //         if (document.getElementById(key).value != itemMaps.get(key)){
-    //             notValid = true
-    //         }
-    //     }
-    //     button.disabled = true
-    // }
-
-    //let input = document.querySelector(".input");
-
-    // let button = document.querySelector(".btn");
-
-    // button.disabled = true
-    // input.addEventListener("change", stateHandle);
-
-    // function stateHandle() {
-    //     if (document.querySelector(".input").value === "") {
-    //         button.disabled = false; //button remains disabled
-    //     } else {
-    //         button.disabled = true; //button is enabled
-    //     }
-    // }
-
-
-    // var input = document.getElementById("item1-3")
-    // if (input.checkValidity()){
-    //     const disableButton = false;
-    // } else {
-    //     const disableButton = true;
-    // }
-
-
-    // const disableButton = true; //change this value to false and the button will be clickable
-    // const button = document.getElementById('submitButton');
-    
-    // if (disableButton) button.disabled = "disabled";
 });
 
 
